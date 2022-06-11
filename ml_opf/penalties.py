918998d32d072576f0c4f40bfd94c85cb81bcf55
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2022-06-11 14:24:10 +0200

    Unify penalty calc

--- ml_opf/penalties.py
+++ ml_opf/penalties.py
@@ -4,54 +4,57 @@ import numpy as np
 # TODO: Does `autocorrect` require an additonal pf afterwards?
 
 
+def calc_penalty(net, unit_type, column, constraint_column, maximal=True,
+                 vectorize=False):
+    values = net[unit_type][column].to_numpy()
+    if 'res_' in unit_type:
+        unit_type = unit_type[4:]
+    constraints = net[unit_type][constraint_column].to_numpy()
+
+    if maximal:
+        violations = values - constraints
+    else:
+        violations = constraints - values
+    violations[violations < 0] = 0.0
+
+    if vectorize:
+        return violations
+
+    return sum(violations)
+
+
 def voltage_violation(net, penalty_factor):
     """ Linear penalty for voltage violations of the upper or lower voltage
     boundary (both treated equally). """
-    # TODO: implement this whole stuff only once: violation(unit_type=bus, column=vm_pu, constrain_column=etc)
-    voltages = net.res_bus.vm_pu.to_numpy()
-    max_voltages = net.bus["max_vm_pu"].to_numpy()
-    min_voltages = net.bus["min_vm_pu"].to_numpy()
 
-    upper_mask = voltages > max_voltages
-    lower_mask = voltages < min_voltages
+    overvoltage = calc_penalty(net, 'res_bus', 'vm_pu', 'max_vm_pu')
+    undervoltage = calc_penalty(net, 'res_bus', 'vm_pu', 'min_vm_pu', False)
 
-    upper_violations = (voltages - max_voltages)[upper_mask].sum()
-    lower_violations = (min_voltages - voltages)[lower_mask].sum()
-    # TODO: use vector instead
-    penalty = (upper_violations + lower_violations) * penalty_factor
-    if upper_violations > 0:
-        print('overvoltage: ', upper_violations * penalty_factor)
-        # print(voltages)
-    if lower_violations > 0:
-        print('undervoltage: ', lower_violations * penalty_factor)
-        # print(voltages)
+    if overvoltage > 0:
+        print('overvoltage: ', overvoltage * penalty_factor)
+    if undervoltage > 0:
+        print('undervoltage: ', undervoltage * penalty_factor)
 
-    return penalty
+    return penalty_factor * (overvoltage + undervoltage)
 
 
-def line_trafo_overload(net, penalty_factor, unit_type: str):
-    loads = net[f'res_{unit_type}'].loading_percent.to_numpy()
-    max_loads = net[unit_type].max_loading_percent.to_numpy()
+def line_trafo_overload(net, penalty_factor, unit_type: str, vectorize=False):
+    violations = calc_penalty(net, f'res_{unit_type}', 'loading_percent',
+                              'max_loading_percent', vectorize=vectorize)
 
-    mask = loads > max_loads
-    violations = (loads - max_loads)[mask].sum()
     if violations > 0:
         print(f'{unit_type} overload: ', violations * penalty_factor)
     return violations * penalty_factor
 
 
-def ext_grid_overpower(net, penalty_factor, column='q_mvar'):
+def ext_grid_overpower(net, penalty_factor, column='q_mvar', vectorize=False):
     """ Linear penalty for violations of max/min active/reactive power from
     external grids. """
-    power = net.res_ext_grid[column].to_numpy()
-    max_power = net.ext_grid[f'max_{column}'].to_numpy()
-    min_power = net.ext_grid[f'min_{column}'].to_numpy()
 
-    upper_mask = power > max_power
-    lower_mask = power < min_power
-
-    upper_violations = (power - max_power)[upper_mask].sum()
-    lower_violations = (min_power - power)[lower_mask].sum()
+    upper_violations = calc_penalty(
+        net, 'res_ext_grid', column, f'max_{column}', vectorize=vectorize)
+    lower_violations = calc_penalty(
+        net, 'res_ext_grid', column, f'min_{column}', False, vectorize=vectorize)
 
     penalty = (upper_violations + lower_violations) * penalty_factor
     if penalty > 0:
