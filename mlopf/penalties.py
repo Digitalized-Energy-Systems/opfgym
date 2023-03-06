76ef6d484acf385a99a75ef3f3904b99afb802f9
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2023-03-06 17:47:50 +0100

    Add diminishing sqare root penalties

--- mlopf/penalties.py
+++ mlopf/penalties.py
@@ -1,6 +1,7 @@
 
 def compute_penalty(net, unit_type: str, column: str, min_or_max: str,
-                    linear_penalty=0, quadr_penalty=0, offset_penalty=0):
+                    linear_penalty=0, quadr_penalty=0, offset_penalty=0,
+                    sqrt_penalty=0):
     """ General function to compute linear, quadratic, anc offset penalties
     for constraint violations in pandapower nets """
 
@@ -16,6 +17,7 @@ def compute_penalty(net, unit_type: str, column: str, min_or_max: str,
 
     penalty = (violations.sum()) * linear_penalty
     penalty += (violations**2).sum() * quadr_penalty
+    penalty += (violations**0.5).sum() * sqrt_penalty
 
     # Penalize every violation with constant factor
     penalty += len(mask) * offset_penalty
