483405f26e9926781f45c8e99579654d8bf34f2e
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2023-07-07 10:57:06 +0200

    Fix penalty tests

--- tests/test_penalties.py
+++ tests/test_penalties.py
@@ -1,49 +1,61 @@
 import pytest
 
-from mlopf.envs.thesis_envs import SimpleOpfEnv
+import pandapower as pp
+import pandapower.networks as pn
+
 import mlopf.penalties as penalties
 
 
 @pytest.fixture
 def net():
-    """ Create a pandapower net with lots of violations. """
-    dummy_env = SimpleOpfEnv()
-    dummy_env.reset(step=0)
-    act = dummy_env.action_space.low
-    dummy_env.step(act)
-    return dummy_env.net
+    net = pn.example_simple()
+    # Define constraints
+    net.bus['max_vm_pu'] = 1.05
+    net.bus['min_vm_pu'] = 0.95
+    net.line['max_loading_percent'] = 100
+    net.trafo['max_loading_percent'] = 100
+    net.ext_grid['min_p_mw'] = -1
+    net.ext_grid['max_p_mw'] = 1
+    net.ext_grid['min_q_mvar'] = -1
+    net.ext_grid['max_q_mvar'] = 1
+    pp.runpp(net)
+    return net
 
 
 def test_voltage_violation(net):
+    net.res_bus.vm_pu[1] = 1.1
     valid, violation, percentage_violation, penalty = penalties.voltage_violation(
-        net)
+        net, linear_penalty=2)
     assert not valid
-    assert penalty >= 0
-    assert violation >= 0
+    assert round(violation, 3) == 0.05
+    assert round(penalty, 3) == -0.1
 
 
 def test_line_overloading(net):
+    net.res_line.loading_percent[1] = 120
     valid, violation, percentage_violation, penalty = penalties.line_overload(
-        net)
+        net, linear_penalty=2)
     assert not valid
-    assert penalty >= 0
-    assert violation >= 0
+    assert violation == 20
+    assert penalty == -40.0
 
 
 def test_trafo_overloading(net):
+    net.res_trafo.loading_percent[0] = 130
     valid, violation, percentage_violation, penalty = penalties.trafo_overload(
-        net)
+        net, linear_penalty=2)
     assert not valid
-    assert penalty >= 0
-    assert violation >= 0
+    assert violation == 30
+    assert penalty == -60.0
 
 
 def test_ext_grid_overpower(net):
+    net.res_ext_grid.q_mvar[0] = 2
     valid, violation, percentage_violation, penalty = penalties.ext_grid_overpower(
-        net, column='q_mvar')
+        net, column='q_mvar', linear_penalty=2)
     assert not valid
-    assert penalty >= 0
-    assert violation >= 0
+    assert violation == 1
+    assert penalty == -2.0
 
 
 def test_compute_penalty():
@@ -61,5 +73,6 @@ def test_compute_penalty():
         violation, n_violations, offset_penalty=1.5, linear_penalty=2)
     assert penalty == -23
 
+
 def test_compute_violation():
-    pass # TODO
+    pass  # TODO
