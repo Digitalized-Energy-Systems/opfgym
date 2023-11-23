bcc5269e999138d4e78af3ba9798477bf34db709
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2023-11-23 12:05:01 +0100

    Bugfix: min_obj is already a vector

--- mlopf/opf_env.py
+++ mlopf/opf_env.py
@@ -442,14 +442,14 @@ class OpfEnv(gym.Env, abc.ABC):
             else:
                 # Make sure that the objective is always positive
                 # This way, even the worst-case results in zero reward
-                objectives += abs(self.min_obj) / len(objectives)
+                objectives += abs(self.min_obj)
         elif self.reward_function == 'replacement_plus_summation':
             # TODO Idea: can these two be combined?!
             # If valid: Use objective as reward
             # If invalid: Use penalties as reward + objective to make sure agent learns both at the same time (and not first only penalties and then only objective))
             # But ensure that the reward is always higher if valid compared to invalid 
             if valids.all():
-                objectives += abs(self.min_obj) / len(objectives)
+                objectives += abs(self.min_obj)
         elif self.reward_function == 'relative':
             # Multiply constraint violation with objective function as penalty
             penalties = -(~valids + percentage_violations)
