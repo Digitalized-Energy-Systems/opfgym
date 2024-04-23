da406906a6eb341de3c4325f300b9cc0d37c37c2
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2024-04-23 14:16:24 +0200

    Improve auto constraint setting for simbench storages (bugfix for VoltageControl env)

--- mlopf/build_simbench_net.py
+++ mlopf/build_simbench_net.py
@@ -1,14 +1,16 @@
+import numpy as np
 import simbench as sb
 
 
-def build_simbench_net(simbench_network_name, gen_scaling=1.0, load_scaling=2.0,
-                       voltage_band=0.05, max_loading=80, *args, **kwargs):
+def build_simbench_net(simbench_network_name, gen_scaling=1.0, load_scaling=1.0,
+                       storage_scaling=1.0, voltage_band=0.05, max_loading=80, 
+                       *args, **kwargs):
     """ Init and return a simbench power network with standard configuration.
     """
 
     net = sb.get_simbench_net(simbench_network_name)
 
-    set_unit_scaling(net, gen_scaling, load_scaling)
+    set_unit_scaling(net, gen_scaling, load_scaling, storage_scaling)
     set_system_constraints(net, voltage_band, max_loading)
 
     assert not sb.profiles_are_missing(net)
@@ -21,7 +23,8 @@ def build_simbench_net(simbench_network_name, gen_scaling=1.0, load_scaling=2.0,
     return net, profiles
 
 
-def set_unit_scaling(net, gen_scaling=1.0, load_scaling=1.0, storage_scaling=1.0):
+def set_unit_scaling(net, gen_scaling=1.0, load_scaling=1.0, 
+                     storage_scaling=1.0):
     net.sgen['scaling'] = gen_scaling
     net.gen['scaling'] = gen_scaling
     net.load['scaling'] = load_scaling
@@ -68,10 +71,14 @@ def set_constraints_from_profiles(net, profiles):
         unit_type, column = type_act
         net_df = net[unit_type]
 
-        net_df[f'max_max_{column}'] = profiles[type_act].max(
-            axis=0) * net_df.scaling
-        net_df[f'min_min_{column}'] = profiles[type_act].min(
-            axis=0) * net_df.scaling
+        if unit_type == 'storage':
+            max_power = np.maximum(profiles[type_act].max(axis=0).abs(),
+                                   profiles[type_act].min(axis=0).abs())
+            net_df[f'max_max_{column}'] = max_power * net_df.scaling
+            net_df[f'min_min_{column}'] = -max_power * net_df.scaling
+        else:
+            net_df[f'max_max_{column}'] = profiles[type_act].max(axis=0) * net_df.scaling
+            net_df[f'min_min_{column}'] = profiles[type_act].min(axis=0) * net_df.scaling
         # Compute mean. Sometimes required for data sampling.
         net_df[f'mean_{column}'] = profiles[type_act].mean(axis=0)
 
@@ -80,7 +87,9 @@ def set_constraints_from_profiles(net, profiles):
         axis=1) - profiles[('sgen', 'p_mw')].sum(axis=1)
     net.ext_grid['max_max_p_mw'] = load_gen_diff.max()
     net.ext_grid['min_min_p_mw'] = load_gen_diff.min()
+    net.ext_grid['mean_p_mw'] = load_gen_diff.mean()
     # Generators should normally not increase q imbalances further
     load_q_mvar = profiles[('load', 'q_mvar')].sum(axis=1)
     net.ext_grid['max_max_q_mvar'] = load_q_mvar.max()
     net.ext_grid['min_min_q_mvar'] = load_q_mvar.min()
+    net.ext_grid['mean_q_mvar'] = load_q_mvar.mean()
