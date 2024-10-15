242f0f3ba498fca4c6191a33ea328dc31386450c
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2024-10-15 10:50:37 +0200

    Add example with discrete (trafos) and continous (reactive power setpoints) actuators for voltage control

--- opfgym/examples/__init__.py
+++ opfgym/examples/__init__.py
@@ -2,3 +2,4 @@ from .network_reconfiguration import NetworkReconfiguration
 from .multi_stage import MultiStageOpf
 from .non_simbench_net import NonSimbenchNet
 from .partial_obs import PartiallyObservable
+from .mixed_continuous_discrete import MixedContinuousDiscrete
