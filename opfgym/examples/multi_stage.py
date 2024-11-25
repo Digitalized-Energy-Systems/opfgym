fcc5245f62784254cf777228a0ded24d9853ed9f
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2024-11-25 10:04:15 +0100

    Add multi-stage base class, see #28

--- opfgym/examples/multi_stage.py
+++ opfgym/examples/multi_stage.py
@@ -3,9 +3,8 @@ Warning: Works only for simbench network because it requires timeseries data.
 Warning: Creates only observation for the current time step, which means that
 the agent has no prediction of the future and can only react to the current
 state. In other words, long-term optimal actions are not necessarily possible
-(and the Markov property is not fulfilled?).
+(and the Markov property is not fulfilled).
 
-TODO: Add this to the base class as a general method to handle multi-stage OPF?
 TODO: Use all steps for observation? Essentially give the agent a prediction.
 TODO: Add a storage system or something similar to actually make the multi-stage aspect relevant.
 
@@ -14,11 +13,11 @@ TODO: Add a storage system or something similar to actually make the multi-stage
 
 import pandapower as pp
 
-from opfgym import opf_env
+from opfgym import MultiStageOpfEnv
 from opfgym.simbench.build_simbench_net import build_simbench_net
 
 
-class MultiStageOpf(opf_env.OpfEnv):
+class MultiStageOpf(MultiStageOpfEnv):
     def __init__(self, simbench_network_name='1-LV-urban6--0-sw',
                  steps_per_episode=4, train_data='simbench',
                  test_data='simbench',
@@ -63,42 +62,6 @@ class MultiStageOpf(opf_env.OpfEnv):
             pp.create_poly_cost(net, idx, 'ext_grid', cp1_eur_per_mw=1)
 
         return net, profiles
-
-    def step(self, action):
-        """ Extend step method to sample the next time step of the simbench data. """
-        obs, reward, terminated, truncated, info = super().step(action)
-
-        new_step = self.current_simbench_step + 1
-
-        # Enforce train/test-split
-        if self.test:
-            # Do not accidentally test on train data!
-            if new_step in self.train_steps:
-                truncated = True
-        else:
-            # And do not accidentally train on test data!
-            if new_step in self.validation_steps or new_step in self.test_steps:
-                truncated = True
-
-        # After n steps = end of episode
-        if self.step_in_episode >= self.steps_per_episode:
-            terminated = True
-
-        if terminated or truncated:
-            return obs, reward, terminated, truncated, info
-
-        # Increment the simbench step
-        self._sampling(step=new_step)
-
-        # Rerun the power flow calculation for the new state if required
-        # TODO: This results in two power flow calculations for each step() call. Is it possible to avoid this?
-        if self.pf_for_obs is True:
-            self.run_power_flow()
-
-        # Create new observation in the new state
-        obs = self._get_obs(self.obs_keys, self.add_time_obs)
-
-        return obs, reward, terminated, truncated, info
 
 
 if __name__ == '__main__':
