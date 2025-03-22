79ff86a3af70987f9cf5b96d577b9ca4aeda77ba
Thomas Wolgast <thomas.wolgast@uni-oldenburg.de>
2025-03-22 15:44:25 +0100

    Release 1.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))  # Adjust '..' to match your project s
 project = 'OPF-Gym'
 copyright = '2025, Thomas Wolgast'
 author = 'Thomas Wolgast'
-release = '1.0.0'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
